878a1c5bcd3200e3afa42285cbd0a0d08077f4c9
pkorchak <paulkorchak@gmail.com>
2021-03-14 14:58:10 +0300

    Completed task

--- src/main/java/com/almworks/internal/interview/issuecache/IssueCacheImpl.java
+++ src/main/java/com/almworks/internal/interview/issuecache/IssueCacheImpl.java
@@ -1,29 +1,135 @@
 package com.almworks.internal.interview.issuecache;
 
-import java.util.Set;
+import org.slf4j.Logger;
+
+import java.util.*;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class IssueCacheImpl implements IssueCache {
-  public IssueCacheImpl(IssueChangeTracker tracker, IssueLoader loader, Set<String> fieldIds) {
 
+  private static final Logger LOG = getLogger(IssueCacheImpl.class);
+
+  private final IssueChangeTracker tracker;
+  private final IssueLoader loader;
+  private final Set<String> fieldIds;
+  private final Map<Long, List<Listener>> issueIdsToListeners = new HashMap<>();
+  private final Map<Long, Map<String, Object>> issueIdsToFieldValues = new HashMap<>();
+
+  public IssueCacheImpl(IssueChangeTracker tracker, IssueLoader loader, Set<String> fieldIds) {
+    this.tracker = tracker;
+    this.loader = loader;
+    this.fieldIds = Set.copyOf(fieldIds); // Converts to an immutable Set
+
+    subscribeToTrackerChanges();
+  }
+
+  private void subscribeToTrackerChanges() {
+    tracker.subscribe(issueIds -> {
+      LOG.debug("Handle changes of issueIds {} from tracker", issueIds);
+      loadIssues(issueIdsToListeners.keySet().stream()
+          .filter(issueIds::contains)
+          .collect(toSet()));
+    });
+  }
+
+  private void loadIssues(Set<Long> issueIds) {
+    if (!issueIds.isEmpty()) {
+      LOG.debug("Call loader for issueIds {}", issueIds);
+      loader.load(issueIds, fieldIds)
+          .thenAccept(result -> result.getIssueIds()
+              .forEach(issueId -> handleIssueChanges(issueId, result.getValues(issueId))));
+    }
+  }
+
+  private void handleIssueChanges(Long issueId, Map<String, Object> newFieldValues) {
+    Map<String, Object> oldFieldValues = issueIdsToFieldValues.get(issueId);
+    Map<String, Object> changedFieldValues = oldFieldValues == null
+        ? newFieldValues
+        : newFieldValues.entrySet().stream() // Skip null check due to the comment above IssueLoader.load
+            .filter(entry -> {
+              Object oldValue = oldFieldValues.get(entry.getKey());
+              Object newValue = entry.getValue();
+
+              // Skip null checks assuming that IssueLoader.getValues never returns a Map with null values
+              // since otherwise it must be fixed in the loader as a design error
+              return !oldValue.equals(newValue);
+            })
+            .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+    if (changedFieldValues.size() > 0) {
+      ofNullable(issueIdsToListeners.get(issueId))
+          .ifPresent(listeners -> {
+            LOG.debug("Add a new or changed issue with id {} and values {} into the cache", issueId, changedFieldValues);
+            listeners.forEach(listener -> listener.onIssueChanged(issueId, changedFieldValues));
+            issueIdsToFieldValues.put(issueId, newFieldValues);
+          });
+    }
   }
 
   @Override
   public void subscribe(Set<Long> issueIds, Listener listener) {
+    LOG.debug("Add a new listener of issueIds {}", issueIds);
+    Set<Long> missingIssueIds = new HashSet<>();
 
+    issueIds.forEach(issueId -> {
+      List<Listener> issueListeners = issueIdsToListeners.get(issueId);
+
+      if (issueListeners == null) {
+        issueIdsToListeners.put(issueId, newArrayList(listener));
+      } else {
+        issueListeners.add(listener);
+      }
+
+      if (issueIdsToFieldValues.containsKey(issueId)) {
+        listener.onIssueChanged(issueId, issueIdsToFieldValues.get(issueId));
+      } else {
+        missingIssueIds.add(issueId);
+      }
+    });
+
+    loadIssues(missingIssueIds);
   }
 
   @Override
   public void unsubscribe(Listener listener) {
+    List<Long> issueIdsWithoutListeners = new ArrayList<>();
 
+    issueIdsToListeners.forEach((issueId, listeners) -> {
+      if (listeners.contains(listener)) {
+        if (listeners.size() > 1) {
+          listeners.remove(listener);
+        } else {
+          issueIdsWithoutListeners.add(issueId);
+        }
+      }
+    });
+
+    issueIdsWithoutListeners.forEach(issueId -> {
+      LOG.debug("The last listener has unsubscribed from issueId {}. Removing from the cache", issueId);
+      issueIdsToListeners.remove(issueId);
+      issueIdsToFieldValues.remove(issueId);
+    });
   }
 
   @Override
   public Object getField(long issueId, String fieldId) {
-    return null;
+    if (fieldId == null || !fieldIds.contains(fieldId)) {
+      LOG.warn("FieldId {} is not stored in the cache", fieldId);
+      return null;
+    }
+
+    return ofNullable(issueIdsToFieldValues.get(issueId))
+        .map(issue -> issue.get(fieldId))
+        .orElse(null);
   }
 
   @Override
   public Set<String> getFieldIds() {
-    return null;
+    return fieldIds;
   }
 }
--- src/test/java/com/almworks/internal/interview/issuecache/IssueCacheTest.java
+++ src/test/java/com/almworks/internal/interview/issuecache/IssueCacheTest.java
@@ -1,14 +1,14 @@
 package com.almworks.internal.interview.issuecache;
 
-import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
-import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -16,69 +16,215 @@ import static org.mockito.Mockito.*;
 
 /**
  * You are expected to write unit tests for Issue Cache.
- * */
+ */
 public class IssueCacheTest {
+
+  private final static Set<String> FIELD_IDS = Set.of("key", "summary");
+
+  private final static Map<String, Object> ISSUE_1 = Map.of("key", "k1", "summary", "s1");
+  private final static Map<String, Object> ISSUE_1_V2 = Map.of("key", "k11", "summary", "s11");
+  private final static Map<String, Object> ISSUE_2 = Map.of("key", "k2", "summary", "s2");
+  private final static Map<String, Object> ISSUE_2_V2 = Map.of("key", "k21", "summary", "s21");
+  private final static Map<String, Object> ISSUE_3 = Map.of("key", "k3", "summary", "s3");
+  private final static Map<String, Object> ISSUE_4 = Map.of("key", "k4", "summary", "s4");
+
+  private final IssueCache.Listener listener1 = mock(IssueCache.Listener.class);
+  private final IssueCache.Listener listener2 = mock(IssueCache.Listener.class);
+
+  private final MockIssueChangeTracker myTracker = new MockIssueChangeTracker();
+  private final MockIssueLoader myLoader = new MockIssueLoader();
+  private final IssueCache myCache = new IssueCacheImpl(myTracker, myLoader, FIELD_IDS);
+
   @Before
   public void setupSubscriptionTo12() {
+    checkCacheDoesNotContain(1L, 2L, 3L, 4L);
     assertThat(myCache.getField(1L, "key"), nullValue());
 
-    myCache.subscribe(Set.of(1L, 2L), listener12);
+    myCache.subscribe(Set.of(1L, 2L), listener1);
 
     assertThat(myLoader.requests, hasSize(1));
+    checkCacheDoesNotContain(1L, 2L, 3L, 4L);
 
-    assertThat(myCache.getField(1L, "key"), nullValue());
+    myLoader.reply(Map.of(1L, ISSUE_1, 2L, ISSUE_2));
 
-    myLoader.reply(Map.of(1L, issue1, 2L, issue2));
+    verify(listener1).onIssueChanged(1L, ISSUE_1);
+    verify(listener1).onIssueChanged(2L, ISSUE_2);
+    verifyNoMoreInteractions(listener1);
+    verifyZeroInteractions(listener2);
+    reset(listener1);
 
-    verify(listener12).onIssueChanged(1L, issue1);
-    verify(listener12).onIssueChanged(2L, issue2);
-    verifyNoMoreInteractions(listener12);
-    reset(listener12);
-
-    assertThat(myCache.getField(1L, "key"), is("k1"));
-    assertThat(myCache.getField(2L, "summary"), is("s2"));
+    // TODO Consider usage of PowerMock to simplify cache state validation
+    checkCacheContains(Map.of(1L, ISSUE_1, 2L, ISSUE_2));
+    checkCacheDoesNotContain(3L, 4L);
   }
 
   @Test
-  public void basic() {
-    // Checks that setupSubscriptionTo12() works
+  public void subscribe_LoadsAllSpecifiedIssuesIfNotStoredInCache() {
+    myCache.subscribe(Set.of(3L, 4L), listener2);
+
+    assertThat(myLoader.requests, hasSize(1));
+    checkRequests(Set.of(3L, 4L));
+
+    myLoader.reply(Map.of(3L, ISSUE_3, 4L, ISSUE_4));
+
+    verify(listener2).onIssueChanged(3L, ISSUE_3);
+    verify(listener2).onIssueChanged(4L, ISSUE_4);
+    verifyNoMoreInteractions(listener2);
+    verifyZeroInteractions(listener1);
+    checkCacheContains(Map.of(1L, ISSUE_1, 2L, ISSUE_2, 3L, ISSUE_3, 4L, ISSUE_4));
   }
 
   @Test
-  public void changeTriggersUpdate() {
+  public void subscribe_LoadsOnlyMissingIssuesIfSomeStoredInCache() {
+    myCache.subscribe(Set.of(2L, 3L), listener2);
+
+    assertThat(myLoader.requests, hasSize(1));
+    checkRequests(Set.of(3L));
+
+    myLoader.reply(Map.of(3L, ISSUE_3));
+
+    verify(listener2).onIssueChanged(2L, ISSUE_2);
+    verify(listener2).onIssueChanged(3L, ISSUE_3);
+    verifyNoMoreInteractions(listener2);
+    verifyZeroInteractions(listener1);
+    checkCacheContains(Map.of(1L, ISSUE_1, 2L, ISSUE_2, 3L, ISSUE_3));
+  }
+
+  @Test
+  public void subscribe_DoesNotLoadIssuesIfAllStoredInCache() {
+    myCache.subscribe(Set.of(1L, 2L), listener2);
+
+    assertThat(myLoader.requests, hasSize(0));
+
+    verify(listener2).onIssueChanged(1L, ISSUE_1);
+    verify(listener2).onIssueChanged(2L, ISSUE_2);
+    verifyNoMoreInteractions(listener2);
+    verifyZeroInteractions(listener1);
+    checkCacheContains(Map.of(1L, ISSUE_1, 2L, ISSUE_2));
+  }
+
+  @Test
+  public void unsubscribe_TheLastListenerOfIssueRemovesItFromCache() {
+    myCache.subscribe(Set.of(1L), listener2);
+    verify(listener2).onIssueChanged(1L, ISSUE_1);
+
+    myCache.unsubscribe(listener1);
+
+    checkCacheContains(Map.of(1L, ISSUE_1));
+    checkCacheDoesNotContain(2L);
+
+    myTracker.recordChanges(Set.of(1L));
+    myLoader.reply(Map.of(1L, ISSUE_1_V2));
+
+    verify(listener2).onIssueChanged(1L, ISSUE_1_V2);
+    verifyNoMoreInteractions(listener1, listener2);
+    checkCacheContains(Map.of(1L, ISSUE_1_V2));
+
+    myCache.unsubscribe(listener2);
+
+    checkCacheDoesNotContain(1L, 2L);
+  }
+
+  @Test
+  public void unsubscribe_DoesNotPutIssueInCacheIfUnsubscribedDuringLoading() {
+    myCache.subscribe(Set.of(3L), listener2);
+
+    assertThat(myLoader.requests, hasSize(1));
+    checkRequests(Set.of(3L));
+    checkCacheDoesNotContain(3L);
+
+    myCache.unsubscribe(listener2);
+    myLoader.reply(Map.of(3L, ISSUE_3));
+
+    verifyZeroInteractions(listener1, listener2);
+    checkCacheDoesNotContain(3L);
+  }
+
+  @Test
+  public void trackerChange_LoadsListenedIssues() {
+    myCache.subscribe(Set.of(1L), listener2);
+    verify(listener2).onIssueChanged(1L, ISSUE_1);
+
     myTracker.recordChanges(Set.of(2L));
 
     assertThat(myLoader.requests, hasSize(1));
     checkRequests(List.of(2L));
-    assertThat(myCache.getField(2L, "key"), is("k2"));
+    checkCacheContains(Map.of(1L, ISSUE_1, 2L, ISSUE_2));
 
-    myLoader.reply(Map.of(2L, issue21));
+    myLoader.reply(Map.of(2L, ISSUE_2_V2));
 
-    verify(listener12).onIssueChanged(2L, issue21);
-    verifyNoMoreInteractions(listener12);
+    verify(listener1).onIssueChanged(2L, ISSUE_2_V2);
+    verifyNoMoreInteractions(listener1, listener2);
+    checkCacheContains(Map.of(1L, ISSUE_1, 2L, ISSUE_2_V2));
 
-    assertThat(myCache.getField(2L, "key"), is("k21"));
+    myTracker.recordChanges(Set.of(1L, 2L));
+
+    assertThat(myLoader.requests, hasSize(1));
+    checkRequests(Set.of(1L, 2L));
+    checkCacheContains(Map.of(1L, ISSUE_1, 2L, ISSUE_2_V2));
+
+    myLoader.reply(Map.of(1L, ISSUE_1_V2, 2L, ISSUE_2));
+
+    verify(listener1).onIssueChanged(1L, ISSUE_1_V2);
+    verify(listener2).onIssueChanged(1L, ISSUE_1_V2);
+    verify(listener1).onIssueChanged(2L, ISSUE_2);
+    verifyNoMoreInteractions(listener1, listener2);
+    checkCacheContains(Map.of(1L, ISSUE_1_V2, 2L, ISSUE_2));
+  }
+
+  @Test
+  public void trackerChange_DoesNotLoadNotListenedIssues() {
+    myTracker.recordChanges(Set.of(3L, 4L));
+
+    assertThat(myLoader.requests, hasSize(0));
+    verifyZeroInteractions(listener1, listener2);
+  }
+
+  @Test
+  public void trackerChange_DoesNotCallListenersIfNoChangesInCachedFields() {
+    myTracker.recordChanges(Set.of(1L, 2L));
+
+    assertThat(myLoader.requests, hasSize(1));
+    checkRequests(Set.of(1L, 2L));
+
+    myLoader.reply(Map.of(1L, ISSUE_1, 2L, ISSUE_2));
+
+    verifyZeroInteractions(listener1, listener2);
+  }
+
+  @Test
+  public void getField_ReturnsNullWhenFieldIdIsNull() {
+    assertThat(myCache.getField(1L, null), nullValue());
+  }
+
+  @Test
+  public void getField_ReturnsNullWhenFieldIdIsUnknown() {
+    assertThat(myCache.getField(1L, "unknownId"), nullValue());
+  }
+
+  @Test
+  public void getFieldIds_Success() {
+    assertThat(myCache.getFieldIds(), is(FIELD_IDS));
   }
 
   @SafeVarargs
-  private void checkRequests(List<Long>... requests) {
-    assertThat(myLoader.requests, IsIterableContainingInOrder.contains(
-      Arrays.stream(requests).map(r ->
-        hasProperty("issueIds", containsInAnyOrder(r.toArray()))
-      ).collect(Collectors.toList())
+  private void checkRequests(Collection<Long>... requests) {
+    assertThat(myLoader.requests, contains(
+        Arrays.stream(requests).map(requestedIssueIds ->
+            hasProperty("issueIds", containsInAnyOrder(requestedIssueIds.toArray()))
+        ).collect(toList())
     ));
   }
 
-  private final MockIssueChangeTracker myTracker = new MockIssueChangeTracker();
-  private final MockIssueLoader myLoader = new MockIssueLoader();
-  private final IssueCache myCache = new IssueCacheImpl(myTracker, myLoader, Set.of("key", "summary"));
+  private void checkCacheContains(Map<Long, Map<String, Object>> issueIdsToFieldValues) {
+    issueIdsToFieldValues.forEach((issueId, fieldValues) ->
+        fieldValues.forEach((fieldId, fieldValue) -> assertThat(myCache.getField(issueId, fieldId), is(fieldValue))));
+  }
 
-  private final IssueCache.Listener listener12 = mock(IssueCache.Listener.class);
-
-  private final Map<String, Object> issue1 = Map.of("key", "k1", "summary", "s1");
-  private final Map<String, Object> issue2 = Map.of("key", "k2", "summary", "s2");
-  private final Map<String, Object> issue21 = Map.of("key", "k21", "summary", "s21");
-
+  private void checkCacheDoesNotContain(Long... issueIds) {
+    Arrays.stream(issueIds).forEach(issueId ->
+        FIELD_IDS.forEach(fieldId -> assertThat(myCache.getField(issueId, fieldId), nullValue())));
+  }
 
   private static class MockIssueChangeTracker implements IssueChangeTracker {
     private IssueChangeTracker.Listener myListener;
@@ -95,7 +241,7 @@ public class IssueCacheTest {
 
     @Override
     public void unsubscribe(Listener listener) {
-      fail("Unexpected call to ubsubscribe()");
+      fail("Unexpected call to unsubscribe()");
     }
   }
 
@@ -124,7 +270,7 @@ public class IssueCacheTest {
     public final CompletableFuture<LoadResult> resultFuture = new CompletableFuture<>();
 
     public IssueLoadRequest(Collection<Long> issueIds, Set<String> fieldIds) {
-      this.issueIds = issueIds.stream().sorted().collect(Collectors.toList());
+      this.issueIds = issueIds.stream().sorted().collect(toList());
       this.fieldIds = fieldIds;
     }
 
